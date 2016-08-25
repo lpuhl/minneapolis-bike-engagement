df6c40478488ae452a248c382cf5d4f59986c22c
Liz Puhl <eapuhl@gmail.com>
2016-08-24 22:09:10 -0500

    added a couple of geoJSON points

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -2,13 +2,49 @@ $(document).ready(function() {
 
 var map = L.map('map',{ center: [44.8611, -93.2933], zoom: 12});
 
-L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
+var bars =  {
+  "type": "FeatureCollection",
+  "features": [
+    { "type": "Feature",
+        "properties": {
+          "amenity": "bar",
+          "name": "Houlihan's"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-93.2858392, 44.8831415]
+        }
+    },
+    { "type": "Feature",
+        "properties": {
+          "amenity": "bar",
+          "name": "Sandy's Tavern",
+          "website": "http://www.sandystavern.com"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-93.3089008, 44.882959]
+        }
+    }]};
 
 var lyn65 = L.marker([44.885933,-93.286542]).addTo(map);
 
 var lineToCoop = L.polyline([[44.885713, -93.286768], [44.886533,  -93.287808]],
     {color: 'red', weight: 10}).addTo(map);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(map);
+
+// L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+// 	maxZoom: 18, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map);
+
+// map.doubleClickZoom.disable();
+
+L.geoJson(bars).addTo(map);
+L.geoJson(bars, {
+    onEachFeature: function (feature, layer) {
+        layer.bindPopup(feature.properties.name);
+    }
+}).addTo(map);
 
 lyn65.bindPopup('<h3><a href="http://lyn65.com">Lyn 65</a></h3>');
 
