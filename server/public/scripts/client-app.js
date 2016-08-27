80e82b6d02694075cd542737b1d09d5fd862d5d6
Liz Puhl <eapuhl@gmail.com>
2016-08-26 21:08:01 -0500

    added a couple of buttons. too tired to do more.

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -52,11 +52,26 @@ var barPoints = L.geoJson(bars, {
 function addBars() {
     barPoints.addTo(map);
 };
+function removeBars() {
+    map.removeLayer(barPoints);
+};
 
+function toggleBars(){
+    if(map.hasLayer(barPoints)){
+        removeBars();
+    } else {
+        addBars();
+    }
+};
 // Create event listener for the Add Coffee Shops Button
 document.getElementById("addButton").addEventListener("click", addBars);
+document.getElementById("removeButton").addEventListener("click", removeBars);
+
+document.getElementById("toggleButton").addEventListener("click", toggleBars);
 
 lyn65.bindPopup('<h3><a href="http://lyn65.com">Lyn 65</a></h3>');
+
+
 
 // var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 // 	maxZoom: 18,
