78f9c140a5761d714bd07eb6b6d5c692df6b2596
Liz Puhl <eapuhl@gmail.com>
2016-09-09 11:23:06 -0500

    leaflet-draw added

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,6 +1,6 @@
 
 // create a map in the "bikemap" div, set the view to a given place and zoom
-L.map("bikemap", {drawControl: true}).setView([44.97, -93.25], 12);
+L.map("map").setView([44.97, -93.25], 12);
 
 // add a MapBox/OSM tile layer
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -8,55 +8,19 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZWxpemFiZXRoIiwiYSI6IkNmdnB1cmMifQ.NlNxa3kOsDxhWJVGxZsPGg'
-}).addTo(bikemap);
+}).addTo(map);
 
 // var bikewaysLayer = new L.GeoJSON.AJAX("../../data/bikeways_lines.geojson");
 // bikewaysLayer.addTo(mymap);
 
-console.log(bikewaysLayer);
+// Initialise the FeatureGroup to store editable layers
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
 
-var lyn65 = L.marker([44.885933,-93.286542]).addTo(mymap);
-lyn65.bindPopup('<h3><a href="http://lyn65.com">Lyn 65</a></h3>');
-
-var mplsBikeCoal = L.marker([44.9733, -93.2478]).addTo(mymap);
-mplsBikeCoal.bindPopup('<h3><a href="http://www.mplsbike.org/">Minneapolis Bicycle Coalition</a></h3><img src="./images/MBC-logo-square-white-bg_bigger.jpg" />');
-
-
-var lineToCoop = L.polyline([[44.885713, -93.286768], [44.886533,  -93.287808]],
-    {color: 'red', weight: 10}).addTo(mymap);
-//
-//
-
-
-
-
-// L.geoJson(bars).addTo(mymap);
-
-// Create event listener for the Add button
-// document.getElementById("addButton").addEventListener("click", addBars);
-// document.getElementById("removeButton").addEventListener("click", removeBars);
-// document.getElementById("toggleButton").addEventListener("click", toggleBars);
-// // set barPoints to a our coffeeShop GeoJSON
-
-// var barPoints = L.geoJson(bars, {
-//     onEachFeature: function (feature, layer) {
-//         layer.bindPopup(feature.properties.name);
-//     }
-// });
-
-
-// add coffee shops function
-// function addBars() {
-//     barPoints.addTo(mymap);
-// };
-// function removeBars() {
-//     map.removeLayer(barPoints);
-// };
-//
-// function toggleBars(){
-//     if(map.hasLayer(barPoints)){
-//         removeBars();
-//     } else {
-//         addBars();
-//     }
-// };
+// Initialise the draw control and pass it the FeatureGroup of editable layers
+var drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+map.addControl(drawControl);
