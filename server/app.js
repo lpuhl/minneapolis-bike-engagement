f8a74d18babb800672b30a12c2de0ce1714ebfe1
Liz Puhl <eapuhl@gmail.com>
2016-09-11 14:32:02 -0500

    ui-leaflet directive working

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
+/** ---------- THIRD PARTY MODULES ---------- **/
 var express = require('express');
 var app = express();
 var path = require('path');
-// var leafletDraw = require('leaflet-draw');
+var bodyParser = require('body-parser');
 
+/** ---------- OUR MODULES ---------- **/
+var bikemap = require('./routes/maproute');
+
+/** ---------- EXPRESS ROUTES ---------- **/
+app.use('/maproute', bikemap);
 
 app.get('/*', function(req, res) {
   var file = req.params[0] || '/views/index.html';
   res.sendFile(path.join(__dirname, './public', file));
 });
 
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+/** ---------- START SERVER ---------- **/
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function() {
---
+++ server/public/scripts/client-config.js
@@ -0,0 +1,16 @@
+var myApp = angular.module('myApp', ['ngRoute', 'nemLogging', 'ui-leaflet']);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: '/views/partials/home-map.html',
+      controller: "mapController"
+    })
+    // .when('/faves', {
+    //   templateUrl: '/views/partials/faves.html',
+    //   controller: "favesController"
+    // })
+    .otherwise({
+      redirectTo: '/home'
+    })
+}]);
